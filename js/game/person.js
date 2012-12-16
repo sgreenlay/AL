20bea69f7fa1411fb4fbf7c7227c1e7f23efeb7a
Scott Greenlay <sgreenlay@gmail.com>
2012-12-15 23:07:38 -0500

    Hour 26

--- js/game/person.js
+++ js/game/person.js
@@ -30,7 +30,7 @@ function LD25Person(x, y) {
 		{
 			task : 'wait',
 			duration : 5000,
-			text : 'Computering'
+			text : 'Computering...'
 		},
 		{
 			task : 'move',
@@ -38,39 +38,59 @@ function LD25Person(x, y) {
 				x : 20,
 				y : 14
 			}
+		},
+		{
+			task : 'wait',
+			duration : 5000,
+			text : 'Computering...'
 		}
 	];
 	this.current_intent = -1;
+	this.act_on_intent = function act_on_intent(level, intent) {
+		if (intent.task === 'move') {
+			this.path = this.find_path(level, this.x, this.y, intent.destination.x, intent.destination.y);
+			if (this.path == null) {
+				return false;
+			}
+		}
+		else if (intent.task === 'wait') {
+			var self = this;
+			this.waiting.duration = intent.duration;
+			this.waiting.condition = function is_door_open(level) {
+				return false;
+			};
+			this.waiting.on_success = function on_success(level) {
+				// should never be called
+			};
+			this.waiting.on_failure = function on_failure(level) {
+				if (typeof intent.text != 'undefined') {
+					self.stop_computer();
+				}
+				if (typeof intent.say != 'undefined') {
+					self.stop_speaking();
+				}
+				self.act_on_next_intent(level);
+			};
+			if (typeof intent.text != 'undefined') {
+				this.computer(intent.text);
+			}
+			if (typeof intent.say != 'undefined') {
+				this.speak(intent.say);
+			}
+		}
+		return true;
+	};
 	this.act_on_next_intent = function act_on_next_intent(level) {
 		if (this.intents.length > 0) {
 			this.current_intent = (this.current_intent + 1) % this.intents.length;
-			var intent = this.intents[this.current_intent];
-			if (intent.task === 'move') {
-				/*
-				this.path = this.find_path(level, this.x, this.y, intent.x, intent.y);
-				if (this.path == null) {
-					// TODO
-				}
-				*/
-			}
-			else if (intent.task === 'wait') {
-				var self = this;
-				this.waiting.duration = intent.duration;
-				this.waiting.condition = function is_door_open(level) {
-					return false;
-				};
-				this.waiting.on_success = function on_success(level) {
-					// should never be called
-				};
-				this.waiting.on_failure = function on_failure(level) {
-					self.stop_speaking();
-					self.act_on_next_intent(level);
-				};
-				this.speak(intent.text);
-			}
+			return this.act_on_intent(level, this.intents[this.current_intent]);
 		}
+		return false;
 	};
 	this.xy_hash = function xy_hash(x, y) {
+		if (typeof x == 'undefined' || typeof y == 'undefined') {
+			console.log("bls");
+		}
 		return x.toString() + ',' + y.toString();
 	};
 	this.generate_graph = function generate_graph(level, x, y) {
@@ -172,19 +192,30 @@ function LD25Person(x, y) {
 	this.stop_speaking = function stop_speaking() {
 		delete this['speech'];
 	};
+	this.computer = function computer(text) {
+		this['compute'] = text;
+	};
+	this.stop_computer = function stop_computer() {
+		delete this['compute'];
+	};
 	this.logic = function logic(engine, elapsed, level) {
-		if (this.waiting.duration > 0) {
+		if (this.current_intent == -1) {
+			this.act_on_next_intent(level);
+		}
+		else if (this.waiting.duration > 0) {
 			if (this.waiting.condition(level)) {
 				this.waiting.duration = 0;
 				if (this.waiting.on_success) {
 					this.waiting.on_success(level);
 				}
 			}
-			this.waiting.duration -= elapsed;
-			if (this.waiting.duration <= 0) {
-				this.waiting.duration = 0;
-				if (this.waiting.on_failure) {
-					this.waiting.on_failure(level);
+			else {
+				this.waiting.duration -= elapsed;
+				if (this.waiting.duration <= 0) {
+					this.waiting.duration = 0;
+					if (this.waiting.on_failure) {
+						this.waiting.on_failure(level);
+					}
 				}
 			}
 		}
@@ -202,63 +233,47 @@ function LD25Person(x, y) {
 			}
 			
 			if (this.offset_x == 0 && this.offset_y == 0) {
-				var intent = intents[this.current_intent];
-				if (intent.type === 'move') {
-					// 
+				if (this.path.length == 0) {
+					this.move_x = 0;
+					this.move_y = 0;
+					this.act_on_next_intent(level);
 				}
-				else if (intent.type === 'wait') {
-					//
-				}
+				else {				
+					var next_point = this.path.shift();
 				
-				var dx = this.move_x;
-				var dy = this.move_y;
+					var dx = next_point.x - this.x;
+					var dy = next_point.y - this.y;
 				
-				var x = this.x + dx;
-				var y = this.y + dy;
-			
-				while ((dx == 0 && dy == 0) || !level.is_valid_coordinates(x, y) || level.is_wall(level.layout[y][x])) {
-					var direction = Math.floor((Math.random() * 4));
-					dx = 0;
-					dy = 0;
-					switch (direction) {
-						case 0:
-							dx = -1;
-							break;
-						case 1:
-							dy = -1;
-							break;
-						case 2:
-							dx = 1;
-							break;
-						case 3:
-							dy = 1;
-							break;
+					var x = this.x + dx;
+					var y = this.y + dy;
+				
+					if (level.is_door_closed(level.layout[y][x])) {
+						var self = this;
+						this.waiting.duration = 7500;
+						this.waiting.condition = function is_door_open(level) {
+							return level.is_door_open(level.layout[y][x]);
+						};
+						this.waiting.on_success = function on_success(level) {
+							self.move_x = dx;
+							self.move_y = dy;
+							self.stop_speaking();
+						};
+						this.waiting.on_failure = function on_failure(level) {
+							self.move_x = 0;
+							self.move_y = 0;
+							
+							level.layout[y][x] = 1;
+							self.act_on_intent(level, self.intents[self.current_intent]);
+							level.layout[y][x] = 3;
+							
+							self.stop_speaking();
+						};
+						this.speak("Open the door AL!");
 					}
-					x = this.x + dx;
-					y = this.y + dy;
-				}
-				
-				if (level.is_door_closed(level.layout[y][x])) {
-					var self = this;
-					this.waiting.duration = 4000;
-					this.waiting.condition = function is_door_open(level) {
-						return level.is_door_open(level.layout[y][x]);
-					};
-					this.waiting.on_success = function on_success(level) {
-						self.move_x = dx;
-						self.move_y = dy;
-						self.stop_speaking();
-					};
-					this.waiting.on_failure = function on_failure(level) {
-						self.move_x = 0;
-						self.move_y = 0;
-						self.stop_speaking();
-					};
-					this.speak("Open the door AL!");
-				}
-				else {
-					this.move_x = dx;
-					this.move_y = dy;
+					else {
+						this.move_x = dx;
+						this.move_y = dy;
+					}
 				}
 			}
 		}
@@ -269,6 +284,9 @@ function LD25Person(x, y) {
 		engine.graphics.draw_sprite('person-live', x_offset, y_offset, level.block_size, level.block_size);
 		if (typeof this['speech'] != 'undefined') {
 			engine.graphics.draw_speech_bubble(x_offset + level.block_size / 2, y_offset, this['speech'], 14, "rgba(100, 100, 100, 1.0)", "rgba(255, 255, 255, 1.0)", 2);
+		}
+		if (typeof this['compute'] != 'undefined') {
+			engine.graphics.draw_speech_bubble(x_offset + level.block_size / 2, y_offset, this['compute'], 14, "rgba(105, 201, 21, 1.0)", "rgba(0, 0, 0, 1.0)", 2);
 		}
 	};
 };
